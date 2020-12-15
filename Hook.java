d7fd13ca89a91631fb06e29a044e48acf61f74b0
Luca Miani <luca.miani@live.de>
2020-12-15 10:01:40 +0100

    rename
    rename und bg anpassung

--- Hook.java
+++ Hook.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Kook here.
+ * Write a description of class Hook here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Kook extends Actor
+public class Hook extends Actor
 {
     /**
-     * Act - do whatever the Kook wants to do. This method is called whenever
+     * Act - do whatever the Hook wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
